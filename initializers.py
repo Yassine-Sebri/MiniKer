76c91321727d3aeb0e4c6f0189b1ad9bf90f4a0b
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-02-13 20:23:21 +0100

    progress

--- initializers.py
+++ initializers.py
@@ -1,8 +1,11 @@
 # Essentials
 import numpy as np
 
+# Xavier initializer for tanh, sigmoid
 def xavier(dim0, dim1):
 	return np.random.uniform(-1, 1, (dim0, dim1)) *  np.sqrt(6.0 / (dim0 + dim1))
+
+# He initializer for Relu
 def he(dim0, dim1):
 	return np.random.uniform(-1, 1, (dim0, dim1)) *  np.sqrt(2.0 / dim0)
 
